10394760d725a8be7bdb6a4fdb96460895b75366
Andrew MacIver <maciver.andrew@gmail.com>
2017-06-18 19:22:46 -0700

    att expresssdb package

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mysql = require('express-database-mysql')
+
 
 
 app.use(express.static('public'))
 // ROUTES: FEEL FREE TO ADD MORE ROUTES AS YOU SEE FIT
+
+// Index route for static page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
 // Route to get cars list
 app.get("/cars", function (req, res) {
   // TODO: Implement getting cars
-  res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 // Route to add car to list
